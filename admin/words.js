94203ad8a0855548a003739064441b0648867929
Frank Joke <frankjoke@hotmail.com>
2020-04-25 14:53:08 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"logic adapter settings": {
+		"en": "Adapter settings for logic",
+		"de": "Adaptereinstellungen für logic",
+		"ru": "Настройки адаптера для logic",
+		"pt": "Configurações do adaptador para logic",
+		"nl": "Adapterinstellingen voor logic",
+		"fr": "Paramètres d'adaptateur pour logic",
+		"it": "Impostazioni dell'adattatore per logic",
+		"es": "Ajustes del adaptador para logic",
+		"pl": "Ustawienia adaptera dla logic",
+		"zh-cn": "logic的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
